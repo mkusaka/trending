844c32fb187b4b79e7f934f3b6dca26d11d5b2a8
mkusaka <hinoshita1992@gmail.com>
2020-01-01 00:01:55 +0900

    feat: add build script, tweak main script, add build for ubuntu binary

--- main.go
+++ main.go
@@ -56,7 +56,10 @@ func GenerateMarkDown(trends []Trend, trendTitle string) string {
 	trendMD := "# " + trendTitle + "\n"
 	for _, trend := range trends {
 		if trendTitle == "general" {
-			trendMD += "- [" + trend.Name + "](" + trend.Url + ") : " + trend.Language + "\n  - " + trend.Description + "\n"
+			trendMD += "- [" + trend.Name + "](" + trend.Url + ") : " + trend.Language + "\n"
+			if trend.Description != "" {
+				trendMD += "  - " + trend.Description + "\n"
+			}
 		} else {
 			trendMD += "- [" + trend.Name + "](" + trend.Url + ")\n  - " + trend.Description + "\n"
 		}
@@ -92,40 +95,64 @@ func URLGenerator(period string) func(language string) string {
 	}
 }
 
-func main() {
+func FetchAndGenerateMarkdown(period string) {
 	c := NewClient(10 * time.Second)
 	targets := append([]string{General}, Languages...)
-	for _, period := range Periods {
-		periodURLGenerator := URLGenerator(period)
-		for _, language := range targets {
-			url := periodURLGenerator(language)
-			fmt.Print(url + "\n")
-			resp, err := c.client.Get(url)
-			if err != nil {
-				log.Fatal("something error")
-			}
 
-			if resp.Body != nil {
-				defer resp.Body.Close()
-				body, err := ioutil.ReadAll(resp.Body)
-				if err != nil {
-					log.Fatal("Body io convert error")
-				}
-				parsed := ParseTrend(string(body))
-				if language == "" {
-					language = "general"
-				}
-				md := GenerateMarkDown(parsed, language)
-				filename := "src/languages/" + language + "/" + period + ".md"
-				f, err := os.Create(filename)
-				if err != nil {
-					log.Fatalf("something wrong with create md file %s", filename)
-				}
-				defer f.Close()
-
-				f.WriteString(md)
-			}
-			time.Sleep(2 * time.Second)
+	periodURLGenerator := URLGenerator(period)
+	for _, language := range targets {
+		url := periodURLGenerator(language)
+		fmt.Print(url + "\n")
+		resp, err := c.client.Get(url)
+		if err != nil {
+			log.Fatal("something error")
 		}
+
+		if resp.Body != nil {
+			defer resp.Body.Close()
+			body, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				log.Fatal("Body io convert error")
+			}
+			parsed := ParseTrend(string(body))
+			if language == "" {
+				language = "general"
+			}
+			md := GenerateMarkDown(parsed, language)
+			filename := "src/languages/" + language + "/" + period + ".md"
+			f, err := os.Create(filename)
+			if err != nil {
+				log.Fatalf("something wrong with create md file %s", filename)
+			}
+			defer f.Close()
+
+			f.WriteString(md)
+		}
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func main() {
+	period := os.Args[1]
+	isValidPeriod := false
+	for _, _period := range Periods {
+		if period == _period {
+			isValidPeriod = true
+			break
+		}
+	}
+	isAllPeriod := false
+	if period == "all" {
+		isAllPeriod = true
+	}
+	if !(isValidPeriod || isAllPeriod) {
+		log.Fatal("invalid Period given. Choose valid period from daily, weekly, monthly or all.")
+	}
+	if isAllPeriod {
+		for _, _period := range Periods {
+			FetchAndGenerateMarkdown(_period)
+		}
+	} else {
+		FetchAndGenerateMarkdown(period)
 	}
 }
